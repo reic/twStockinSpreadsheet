10aeb1e84a6a2d45cc6dedd831074ac77fcf057f
Reic Wang <reic.wang@gmail.com>
2020-03-30 14:19:05 +0800

    Update spreadsheetMacro.js
    近期因為逐筆交易， json 查詢股票資料沒有很即時。
    目前作法，當缺乏即時交易資料時，取買入 5 筆之最高價。若買入5筆資料也不存在時，取昨加收盤價。

--- spreadsheetMacro.js
+++ spreadsheetMacro.js
@@ -31,13 +31,20 @@ function setStocksPrice() {
         j++; 
       }          
 // obj.z 為最近的成交價 obj.y 昨日收盤價
-      if (stockPriceDetail[i].z ="-")
+// obj.y 昨日收盤價
+// obj.b 買入 5 檔，取最高的買入價
+      if (stockPriceDetail[i].z =="-")
       {
-        spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].y);   
-      }else
+        if(spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].b =="-"))
+        {
+             spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].y);
+        }else{
+             spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].b.split("_",1));  
+        }
+      } else
       {
         spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].z);   
-      }  
+      }   
     }
     spreadsheet.getRange('A3').setValue("完成更新");
   }
@@ -74,13 +81,20 @@ function retry_setStocksPrice() {
         j++; 
       }
 // obj.z 為最近的成交價 obj.y 昨日收盤價
-      if (stockPriceDetail[i].z ="-")
+// obj.y 昨日收盤價
+// obj.b 買入 5 檔，取最高的買入價
+      if (stockPriceDetail[i].z =="-")
       {
-        spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].y);   
-      }else
+        if(spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].b =="-"))
+        {
+             spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].y);
+        }else{
+             spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].b.split("_",1));  
+        }
+      } else
       {
         spreadsheet.getCurrentCell().offset(i+j,0).setValue(stockPriceDetail[i].z);   
-      }   
+      }    
     }
 // 重置 M1, M2 儲存格
     spreadsheet.getRange('M2').setValue('');
